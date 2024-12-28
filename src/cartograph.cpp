e4bdcaf63bd6463281abec543856ceec09210da3
Trim Bresilla <trim.bresilla@gmail.com>
2024-12-28 03:48:04 +0100

    style: refactor codebase for improved readability
    - Remove an unnecessary newline in `cartograph.cpp`

--- src/cartograph.cpp
+++ src/cartograph.cpp
@@ -6,7 +6,6 @@
 #include "farmbot_cartograph/geotiff.hpp"
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include <spdlog/spdlog.h>
-
 namespace echo = spdlog;
 
 class GeoParserNode {
